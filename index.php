81e7660e1d8f273e9a45204e4f1238aebef221ff
Nathasya Lonteng <nathasyaltg@gmail.com>
2022-10-27 05:56:36 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo "hello world<br>";
+
+
+    $nama = "Nathasya";
+    $umur = 20;
+
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur <br>";
+
+    $namaAdik = "Patricia";
+    $umurAdik = 17;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik<br>";
+
+
+    $selisihUmur = $umur - $umurAdik;
+
+    echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+
+?>
